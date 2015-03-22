7d607a5517182964fd8bb0c0b99b9f414c1f5d3d
Jonathan Bastien-Filiatrault <joe@x2a.org>
2015-03-22 15:57:03 -0400

    Fix build.

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -1,5 +1,5 @@
 #include "Constants.h"
 
 const float GlobalConstants::pressure_water = 0.0627;
-const float GlobalConstants::initialAmbienPressure = 1.013;
+const float GlobalConstants::initial_Ambient_Pressure = 1.013;
 const float GlobalConstants::NITROGEN_AIR = 0.79;
