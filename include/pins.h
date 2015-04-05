80d1cef030300c841cb55f7a675f60f3c16113cd
Jonathan Bastien-Filiatrault <joe@x2a.org>
2015-04-05 12:45:03 -0400

    Rejiggage des définitions de pins.

--- include/pins.h
+++ include/pins.h
@@ -8,17 +8,20 @@
 typedef struct {
 	GPIO_TypeDef *port;
 	u32 number;
+	u8 af;
 } pin_t;
 
+#define PIN_DEF(NAME, PORT, PIN) static const pin_t NAME = {.port=PORT, .number=PIN, .af=0};
+#define PIN_DEF_AF(NAME, PORT, PIN, AF) static const pin_t NAME = {.port=PORT, .number=PIN, .af=AF};
+
 // Prototype 1
-extern const pin_t DEPTH_MOSI_PIN;
-extern const pin_t DEPTH_MISO_PIN;
-extern const pin_t DEPTH_SCK_PIN;
-extern const pin_t DEPTH_CS_PIN;
+PIN_DEF_AF(DEPTH_MOSI_PIN, GPIOB, GPIO_PIN_15, GPIO_AF5_SPI2);
+PIN_DEF_AF(DEPTH_MISO_PIN, GPIOC, GPIO_PIN_2, GPIO_AF5_SPI2);
+PIN_DEF_AF(DEPTH_SCK_PIN, GPIOB, GPIO_PIN_13, GPIO_AF5_SPI2);
+PIN_DEF(DEPTH_CS_PIN, GPIOB, GPIO_PIN_14);
 
 #define DEPTH_SPI SPI2
 #define DEPTH_SPI_ENABLE __SPI2_CLK_ENABLE
 #define DEPTH_SPI_DISABLE __SPI2_CLK_DISABLE
-#define DEPTH_SPI_AF GPIO_AF5_SPI2
 
 #endif
