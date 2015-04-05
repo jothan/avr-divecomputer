5ba390539bfa978723ada0e5ecbae66c26159cd1
Marc Collin <marccollin7379@gmail.com>
2015-04-05 16:09:30 -0400

    Set variable to 0

--- src/CompartmentTissue.cpp
+++ src/CompartmentTissue.cpp
@@ -8,11 +8,17 @@
 #include "CompartmentTissue.h"
 
 CompartmentTissue::CompartmentTissue() {
-	// TODO Auto-generated constructor stub
-
+    kHe = 0;
+    kN2 = 0; // Time constants - calculated from halftimes
+    aHe = 0;
+    bHe = 0;
+    aN2 = 0;
+    bN2 = 0; // A and b co-efficients
+    ppHe = 0;
+    ppN2 = 0;
 }
 
 CompartmentTissue::~CompartmentTissue() {
-	// TODO Auto-generated destructor stub
+    // TODO Auto-generated destructor stub
 }
 
